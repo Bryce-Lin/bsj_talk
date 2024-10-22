774764d6f3839dd8053b96afadc026a9f7cc7a99
Bryce-Lin <771221738@qq.com>
2024-10-22 17:04:26 +0800

    add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+import { Suspense } from 'react'
 import { Component } from "@/components/component";
 
-export default function HomePage() {
+export default function Page() {
   return (
-    <>
+    <Suspense fallback={<div>加载中...</div>}>
       <Component />
-    </>
+    </Suspense>
   );
 }
